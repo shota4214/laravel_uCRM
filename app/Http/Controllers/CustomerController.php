edaa0dfa9096a25fbb3b90c7882da7471af50e06
shota_suzuki <seshiruff4214@yahoo.co.jp>
2023-06-21 09:48:04 +0900

    customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
@@ -14,14 +15,19 @@ class CustomerController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         // $getCustomers = Customer::select('id', 'name', 'kana', 'tel')->get();
         // $getPaginate = Customer::select('id', 'name', 'kana', 'tel')->paginate(50);
         // dd($getCustomers, $getPaginate);
 
+        $customers = Customer::searchCustomers($request->search)
+        ->select('id', 'name', 'kana', 'tel')->paginate(50);
+
+        // dd($customers);
+
         return Inertia::render('Customers/Index', [
-            'customers' => Customer::select('id', 'name', 'kana', 'tel')->paginate(50)
+            'customers' => $customers
         ]);
     }
 
@@ -32,7 +38,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Customers/Create');
     }
 
     /**
@@ -43,7 +49,24 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+
+        Customer::create([
+            'name' => $request->name,
+            'kana' => $request->kana,
+            'tel' => $request->tel,
+            'email' => $request->email,
+            'postcode' => $request->postcode,
+            'address' => $request->address,
+            'birthday' => $request->birthday,
+            'gender' => $request->gender,
+            'memo' => $request->memo,
+        ]);
+
+        return to_route('customers.index')
+        ->with([
+            'message' => '登録しました。',
+            'status' => 'success'
+        ]);
     }
 
     /**
--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -17,7 +17,7 @@ defineProps({
         <Link v-else
         class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded
         hover:bg-white focus:border-indigo-500 focus:text-indigo-500"
-        :class="{ 'bg-blue-700 text-white': link.active }" :href="link.url" vhtml="
+        :class="{ 'bg-blue-700 text-white': link.active }" :href="link.url" v-html="
         link.label" />
       </template>
     </div>
