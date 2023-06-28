bcd00cd98fb39761d8bb5c6c59dfc0a8172ee4b1
shota_suzuki <seshiruff4214@yahoo.co.jp>
2023-06-28 22:52:22 +0900

    customerSearch

--- resources/js/Pages/Purchases/Create.vue
+++ resources/js/Pages/Purchases/Create.vue
@@ -8,7 +8,7 @@
   import MicroModal from '@/Components/MicroModal.vue'
 
   const props = defineProps({
-      'customers' : Array,
+      // 'customers' : Array,
       'items' : Array
     })
 
@@ -56,6 +56,11 @@
   }
 
   const quantity = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] // option用
+
+  const setCustomerId = id => {
+    form.customer_id = id
+  }
+
 </script>
 
 <template>
@@ -86,13 +91,13 @@
 
                       <div class="p-2 w-full">
                         <div class="relative">
-                          <MicroModal />
                           <label for="customer" class="leading-7 text-sm text-gray-600">会員名</label>
-                          <select name="customer" v-model="form.customer_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                          <MicroModal @update:customerId="setCustomerId"/>
+                          <!-- <select name="customer" v-model="form.customer_id" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             <option v-for="customer in customers" :value="customer.id" :key="customer.id">
                               {{ customer.id }} : {{ customer.name }}
-                            </option>
-                          </select>
+                            </option> 
+                          </select> -->
                         </div>
                       </div>
 
@@ -126,7 +131,7 @@
                         </div>
 
                       <div class="p-2 w-full">
-                        <div class="relative">
+                        <div class="">
                           <label for="price" class="leading-7 text-sm text-gray-600">合計金額</label>
                           <div class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">合計：{{ totalPrice }}円</div>
                         </div>
